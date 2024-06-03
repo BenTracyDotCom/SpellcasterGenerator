2550b10841e8a28c036cd76e31f3ffbd3f1a44f9
Ben Tracy <b.rob.tracy@gmail.com>
2024-06-03 13:40:02 -0400

    Add slot tracking and clickable spells to npc page

--- src/screens/Npc.jsx
+++ src/screens/Npc.jsx
@@ -1,13 +1,33 @@
 import { View, TouchableOpacity, Text } from "react-native";
+//SlotInfo gets Level, MaxSlots
+import SlotInfo from "../features/spells/SlotInfo";
+import ClickableSpell from "../components/ClickableSpell";
 
-export default function Npc({ route }) {
 
+export default function Npc({ route, navigation }) {
+
+  
   const { npc } = route.params
 
   return (
     <View>
       <Text>NPC goes here!</Text>
-      <Text>{JSON.stringify(npc.name)}</Text>
+      {npc.slots ? npc.slots.map((slot, i) => {
+        if(slot && i > 0){
+          return(
+            <View>
+              <SlotInfo level={i} maxSlots={slot} key={i}/>
+              {npc.prepared[i] ? npc.prepared[i].map((spell, i) => (
+                <ClickableSpell navigation={navigation} spell={spell}/>
+                // <Text key={i}>{spell.name}</Text>
+      )) : null}
+            </View>
+
+          )
+        }
+      }
+
+      ) : null}
     </View>
   )
 }
