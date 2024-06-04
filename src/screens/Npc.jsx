55e36de59fa0fdfce4825d7f717a4bbd417e21a9
Ben Tracy <b.rob.tracy@gmail.com>
2024-06-04 11:44:38 -0400

    add cantrips to npc screen

--- src/screens/Npc.jsx
+++ src/screens/Npc.jsx
@@ -6,20 +6,27 @@ import ClickableSpell from "../components/ClickableSpell";
 
 export default function Npc({ route, navigation }) {
 
-  
+
   const { npc } = route.params
 
   return (
     <View>
-      {npc.slots ? npc.slots.map((slot, i) => {
-        if(slot && i > 0){
-          return(
-            <View>
-              <SlotInfo level={i} maxSlots={slot} key={i}/>
-              {npc.prepared[i] ? npc.prepared[i].map((spell, i) => (
-                <ClickableSpell navigation={navigation} spell={spell}/>
-                // <Text key={i}>{spell.name}</Text>
+      {npc.prepared[0] ? 
+      <Text className="text-xl font-bold border-b-2">Cantrips</Text>
+    : null}
+      {npc.slots ? npc.prepared[0].map(cantrip => (
+        <ClickableSpell navigation={navigation} spell={cantrip} />
       )) : null}
+      {npc.slots ? npc.slots.map((slot, i) => {
+        if (slot && i > 0) {
+          return (
+            <View>
+                    <Text className="text-xl font-bold border-y-2">{`Level ${i} Spells`}</Text>
+              <SlotInfo level={i} maxSlots={slot} key={i} />
+              {npc.prepared[i] ? npc.prepared[i].map((spell, i) => (
+                <ClickableSpell navigation={navigation} spell={spell} />
+                // <Text key={i}>{spell.name}</Text>
+              )) : null}
             </View>
 
           )
